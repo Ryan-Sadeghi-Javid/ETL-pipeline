53cbbb6c6d33425ac9dfb74226556d06a9aba148
Ryan-Sadeghi-Javid <javidryan46@gmail.com>
2025-04-04 19:57:55 -0400

    added comments - got rid of bloat!

--- Graph.cpp
+++ Graph.cpp
@@ -3,6 +3,7 @@
 #include<queue>
 #include <cmath>
 
+// Function to initalize all nodes of the graph with current country storage data
 void Graph::initalize(CountryTotal countires[512]) {
 
     // Get rid of existing graph
@@ -10,7 +11,9 @@ void Graph::initalize(CountryTotal countires[512]) {
     adj_list.clear();
     relationship.clear();
 
+    // Loop through country storage array
     for(int i = 0; i <512;i++) {
+        // Add countries that aren't empty or have been removed ("EEE" -> removed)
         if(!countires[i].empty() && countires[i].get_country_name() != "EEE") {
             node_map[countires[i].get_country_code()] = countires[i];
         }
@@ -18,116 +21,131 @@ void Graph::initalize(CountryTotal countires[512]) {
     std::cout << "success" << std::endl;    
 }
 
+// Update edges based on a certain reltionship inputted by the user
 void Graph::update_edges(std::string series_code, double threshold, std::string relation) {
-    bool edges_added = false;
+    bool edges_added = false; // Tracking if a min of one edge was added
     for(std::unordered_map<std::string, CountryTotal>::iterator i = node_map.begin(); i!= node_map.end(); i++) {
         for(std::unordered_map<std::string, CountryTotal>::iterator j = node_map.begin(); j!=node_map.end(); j++) {
-            if(i == j) {
-                continue;
-            }
 
+            // Getting the two country objects to compare
             CountryTotal& country_1 = i->second;
             CountryTotal& country_2 = j->second;
 
+            // Getting the time series of the respective countires
             TimeSeries time_series_1 = country_1.get_time_series(series_code);
             TimeSeries time_series_2 = country_2.get_time_series(series_code);
 
+            // Getting the mean of the respective time series
             double mean_1 = time_series_1.mean();
             double mean_2 = time_series_2.mean();
 
+            // If data invalid -> SKIP!
             if(mean_1==-1 || mean_2==-1) {
                 continue;
             }
 
-            bool relation_one = false;
-            bool relation_two = false;
+            // Checking if both conditios are met!
+            bool condition_one = false;
+            bool condition_two = false;
 
+            // Checking if the condition's are met based on user input
             if(relation=="greater") {
                 if(mean_1 > threshold) {
-                    relation_one = true;
+                    condition_one = true;
                 }
                 if(mean_2 > threshold) {
-                    relation_two = true;
+                    condition_two = true;
                 }
             }
             else if(relation=="equal") {
                 if(std::fabs(mean_1-threshold) < 1E-3) {
-                    relation_one = true;
+                    condition_one = true;
                 }
                 if(std::fabs(mean_2-threshold) < 1E-3) {
-                    relation_two = true;
+                    condition_two = true;
                 }
             } else if(relation=="less"){
                 if(mean_1 < threshold) {
-                    relation_one = true;
+                    condition_one = true;
                 }
                 if(mean_2 < threshold) {
-                    relation_two = true;
+                    condition_two = true;
                 }
             }
 
-            if(relation_one && relation_two) {
+            // If both conditions are met, add edge!
+            if(condition_one && condition_two) {
                 std::string series_code_1 = i->first;
                 std::string series_code_2 = j->first;
                 std::tuple<std::string, double, std::string> relation_tuple = std::make_tuple(series_code,threshold,relation);
+                // Add if reltionships hasn't already been added!!!!
                 if(relationship[series_code_1][series_code_2].count(relation_tuple) == 0 && relationship[series_code_2][series_code_1].count(relation_tuple) == 0) {
+                    // Undirected graph, therefore add edge to both nodes
                     relationship[series_code_1][series_code_2].insert(relation_tuple);
                     relationship[series_code_2][series_code_1].insert(relation_tuple);
                     adj_list[series_code_1].insert(series_code_2);
                     adj_list[series_code_2].insert(series_code_1);
-                    edges_added = true;
+                    edges_added = true; // If at least one edge was added, update condition
                 }
             }
         }
 
     }
-    if(edges_added) {
+    if(edges_added) { // Print success if at least one edge was added!
         std::cout << "success" << std::endl;
     } else{
         std::cout << "failure" << std::endl;
     }
 }
 
+// Function to print out all countires adjacent (connected by one edge) to a user inputted country code
 void Graph::adjacent(std::string country_code) {
     std::unordered_map<std::string, CountryTotal>::iterator i = node_map.find(country_code);
+    // If country is not in the graph, print out "failure"
     if(i == node_map.end()) {
         std::cout << "failure" << std::endl;
         return;
     }
     std::unordered_map<std::string,std::set<std::string>>::iterator adj_list_i = adj_list.find(country_code);
+    // If country is in the graph but has NO adjacent connections, print out "none"
     if(adj_list_i == adj_list.end() || adj_list_i->second.empty()) {
         std::cout << "none" <<std::endl;
         return;
     }
+    // iterate over all adjacent country codes
     std::set<std::string> adj_set_i = adj_list_i->second;
     for(std::set<std::string>::iterator j = adj_set_i.begin(); j != adj_set_i.end(); j++) {
+        // Find the assoisted country object for the adjacent country code
         std::unordered_map<std::string, CountryTotal>::iterator adj = node_map.find(*j);
         if(adj != node_map.end()) {
-            std::cout << adj->second.get_country_name() << " ";
+            std::cout << adj->second.get_country_name() << " "; // Print name of said adjacent country
         }
     }
     std::cout<<std::endl;
 }
 
+// Function to check if a path exists between two countries in the graph using BFS
 void Graph::path(std::string country_code_1, std::string country_code_2) {
-    if(node_map.find(country_code_1) == node_map.end() || node_map.find(country_code_2) == node_map.end()) {
-        std::cout << "false" << std::endl;
-        return;
-    }
+    // Keep track of visited nodes
     std::set<std::string> visited;
+    
+    // queue used for classic bfs traversal
     std::queue<std::string> queue;
     queue.push(country_code_1);
     visited.insert(country_code_1);
+
+    // keep going until all nodes have been visited that are reachable
     while(!queue.empty()) {
-        std::string current_node = queue.front();
+        std::string current_node = queue.front(); // Get cur node
         queue.pop();
-        if(current_node == country_code_2) {
+        if(current_node == country_code_2) { // Check if cur node is target country
             std::cout << "true" << std::endl;
             return;
         }
+        // Check all adjacent nodes to the current node
         if(adj_list.find(current_node) != adj_list.end()) {
             for(std::set<std::string>::iterator i = adj_list.find(current_node)->second.begin(); i!=adj_list.find(current_node)->second.end(); i++ ) {
-                if(visited.find(*i) == visited.end()) {
+                if(visited.find(*i) == visited.end()) { // If adj node hasn't been visited, add to queue and visited
                     queue.push(*i);
                     visited.insert(*i);
                 }
@@ -141,13 +159,16 @@ void Graph::path(std::string country_code_1, std::string country_code_2) {
     return;
 }
 
+// Function that prints out relationships (tuple) between two countries in the graph
 void Graph::relationships(std::string country_code_1, std::string country_code_2) {
-    if(relationship.find(country_code_1) != relationship.end() && relationship.at(country_code_1).find(country_code_2) != relationship.at(country_code_1).end() ) {
+    if(relationship.find(country_code_1) != relationship.end() && relationship.at(country_code_1).find(country_code_2) != relationship.at(country_code_1).end() ) { // Check if there are any reltionship tuples between the two countries
         for(std::set<std::tuple<std::string, double, std::string>>::iterator i = relationship.at(country_code_1).at(country_code_2).begin(); i != relationship.at(country_code_1).at(country_code_2).end(); i++) {
+            // Extract data from relationship tuples
             std::string series_code = std::get<0>(*i);
             double threshold = std::get<1>(*i);
             std::string relation = std::get<2>(*i);
-
+            
+            // Print out said extracted data
             std::cout << "(" << series_code << " " << threshold << " " << relation << ") ";
         }
     } else {
--- Graph.h
+++ Graph.h
@@ -9,10 +9,12 @@
 
 class Graph {
     private:
-        std::unordered_map<std::string, CountryTotal> node_map;
-        std::unordered_map<std::string, std::set<std::string>> adj_list;
-        std::unordered_map<std::string, std::unordered_map<std::string,std::set<std::tuple<std::string, double, std::string>>>> relationship;
+        std::unordered_map<std::string, CountryTotal> node_map; // Each node's data
+        std::unordered_map<std::string, std::set<std::string>> adj_list; // Adjancy list that maps a country code to its corresponding country object
+        std::unordered_map<std::string, std::unordered_map<std::string,std::set<std::tuple<std::string, double, std::string>>>> relationship; // Maps a pair of countries to a set of tuples that represents the series code
     public:
+
+        // Each of the functions in the design document
         void initalize(CountryTotal countires[512]);
         void update_edges(std::string series_code, double threshold, std::string relation);
         void adjacent(std::string country_code);
